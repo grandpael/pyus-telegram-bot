ee1a8e17d715f3ea692d631358282df7cd7f14cc
grandpael <elvinzet@bk.ru>
2023-03-26 13:46:25 +0500

    Done!

--- package/pyrun.py
+++ package/pyrun.py
@@ -1,15 +1,14 @@
 from settings import *
 
-@dp.message(Command('pyrun'))
+@dp.message(Command('run'))
 async def cmd_pyrun(message: types.Message):
 
-    args = " ".join(message.text.split()[1:])
+    msd = [message.text[5:]]
+    args = " ".join(msd)
     with open("package/pyrun_launch.py", "w", encoding='utf-8') as myfile:
         myfile.write(str(args))
-
     try:
         r = subprocess.check_output('python package/pyrun_launch.py', shell=True, stderr=subprocess.STDOUT, stdin=False)
         await message.answer(r)
     except subprocess.CalledProcessError as e:
-        print(e.output)
-        await message.answer(f'error\n {e.output}')
+        await message.answer(e.output.decode())
